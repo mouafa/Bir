85a798d74de4043a15bbca94e4f9645b4eadd89a
mouafa ahmed <mouafa.ahmed@gmail.com>
2017-01-22 01:48:14 +0000

    feat(new test version):

--- test/index.js
+++ test/index.js
@@ -3,8 +3,18 @@ var bus = require('../src')
 
 test('test general use', t => {
     var x = 0
-    bus.on('test', () => x = 4)
+    bus.on('test', () => x = 2)
+    bus.on('test', () => x = x*2)
     bus.emit('test')
     t.is(x, 4)
+    //t.pass();
+})
+
+test('test once use', t => {
+    var x = 0
+    bus.once('test', () => x = ++x)
+    bus.emit('test')
+    bus.emit('test')
+    t.is(x, 1)
     //t.pass();
 })
