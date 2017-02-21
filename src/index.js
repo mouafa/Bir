747a257122319a16f7fb5cf726bda16b62565e86
mouafa ahmed <mouafa.ahmed@gmail.com>
2017-02-21 11:02:55 +0000

    feat(enhance the API with ES15 class):

--- src/index.js
+++ src/index.js
@@ -1,61 +1,82 @@
-var listners = {}
+'use strict'
 
-const on = (id, fn) => {
-  if (listners[id]) listners[id].push(fn)
-  else listners[id] = [fn]
-    return {
-    remove: remove.bind(null, id, fn),
-    replace: replace.bind(null, id, fn)
+class Bir {
+  constructor() {
+    this.canals = {}
   }
-}
-
-const remove = (id, fn) => {
-  const index = listners[id].indexOf(fn)
-  if (~index) listners[id].splice(index, 1)
-}
-
-const once = (id, fn) => {
-  function fnBox (...args) {
-    fn(...args)
-    remove(id, fnBox)
+  
+  on(id, fn) {
+    if (this.canals[id]) this.canals[id].push(fn)
+    else this.canals[id] = [fn]
+    return Object.assign(this, {
+      off: this.off.bind(this, id, fn),
+      replace: this.replace.bind(this, id, fn),
+    })
   }
-  return on(id, fnBox)
-}
-
-const debounce = (id, fn, time = 0) => {
-  let timer
-  function fnBox (...args) {
-    clearTimeout(timer)
-    timer = setTimeout(fn.bind(null, ...args), time)
+  
+  off(id, fn) {
+    const index = this.canals[id].indexOf(fn)
+    if (~index) this.canals[id].splice(index, 1)
+    return this
   }
-  return on(id, fnBox)
-}
-
-const replace = (id, fn, newFn) => {
-  const index = listners[id].findIndex(fn)
-  listners[id] = newFn
-}
-
-const emit = (id, ...args) => {
-  const _listners = listners[id]
-  if (!_listners) return
-  _listners.map(fn => fn(...args))
-}
-
-const clear = (id) => {
-  if (typeof id === 'string') {
-    delete listners.id
-  } else {
-    listners = {}
+  
+  once(id, fn) {
+    const fnBox = (...args) => {
+      fn(...args)
+      this.off(id, fnBox)
+    }
+    return this.on(id, fnBox)
   }
+  
+  
+  clear(id) {
+    this.canals[id] = []
+    return this
+  }
+
+  debounce(id, fn, time = 0) {
+    let timer
+    function fnBox (...args) {
+      clearTimeout(timer)
+      timer = setTimeout(fn.bind(null, ...args), time)
+    }
+    return this.on(id, fnBox)
+  }
+
+  replace(id, fn, newFn) {
+    const index = this.canals[id].findIndex(fn)
+    if (~index) this.canals[id][index] = newFn
+    return this
+  }
+  
+  emit(id, ...args) {
+    const callbacks = this.canals[id]
+    if (callbacks) callbacks.map(fn => fn(...args))
+    return this
+  }
+  
+  broadcast(id, ...args) {
+   if (this === root) {
+     this.emit(id, ...args)
+     return this
+    }
+    this.emit(id, ...args)
+    this.emit.call(root, id, ...args)
+    return this
+  }
+  
+  canal() {
+    return this
+  }
+
 }
 
-module.exports = {
-  on,
-  remove,
-  once,
-  emit,
-  replace,
-  debounce,
-  clear
+const root = new Bir()
+console.log('root', root)
+
+function BirWrapper() {
+  if (!new.target) return root
+  return new Bir()
 }
+
+module.exports = BirWrapper
--- test/index.js
+++ test/index.js
@@ -1,20 +1,78 @@
 import test from 'ava'
-var bus = require('../src')
+const Bir = require('../src')
 
-test('test general use', t => {
-    var x = 0
-    bus.on('test', () => x = 2)
-    bus.on('test', () => x = x*2)
-    bus.emit('test')
+test('test on use case', t => {
+    const local = new Bir() 
+    let x = 0
+    local.on('test', () => x = 2)
+    local.on('test', () => x = x*2)
+    local.emit('test')
     t.is(x, 4)
-    //t.pass();
 })
 
-test('test once use', t => {
-    var x = 0
-    bus.once('test', () => x = ++x)
-    bus.emit('test')
-    bus.emit('test')
+test('test once use case', t => {
+    const local = new Bir() 
+    let x = 0
+    local.once('test', () => x = ++x)
+    local.emit('test')
+    local.emit('test')
     t.is(x, 1)
-    //t.pass();
 })
+
+test('test off use case', t => {
+    const local = new Bir() 
+    let x = true
+    const callback =  () => x = !x
+    local.on('test', callback)
+    local.emit('test')
+    local.off('test', callback)
+    local.emit('test')
+    t.is(x, false)
+})
+
+test('test binded off use case', t => {
+    const local = new Bir() 
+    let x = true
+    const listener = local.on('test', () => x = !x)
+    local.emit('test')
+    listener.off()
+    local.emit('test')
+    t.is(x, false)
+})
+
+test('test chained off use case', t => {
+    const local = new Bir() 
+    let x = true
+    local.on('test', () => x = !x).off()
+    local.emit('test')
+    t.is(x, true)
+})
+
+// test('test replace use case', t => {
+//     const local = new Bir() 
+//     let x = true
+//     const callback =  () => x = !x
+//     local.on('test', callback)
+//     local.emit('test')
+//     local.replace('test', callback, () => x = 'replaced')
+//     local.emit('test')
+//     t.is(x, 'replaced')
+// })
+
+// test('test binded replace use case', t => {
+//     const local = new Bir() 
+//     let x = true
+//     const listener = local.on('test', () => x = !x)
+//     local.emit('test')
+//     listener.replace(() => x = 'replaced')
+//     local.emit('test')
+//     t.is(x, 'replaced')
+// })
+
+// test('test chained replace use case', t => {
+//     const local = new Bir() 
+//     let x = true
+//     local.on('test', () => x = !x).replace(() => x = 'replaced')
+//     local.emit('test')
+//     t.is(x, 'replaced')
+// })
